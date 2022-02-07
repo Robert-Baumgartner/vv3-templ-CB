f7ee01d0ef93b686580675eb9fdb93612562aff6
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2022-02-07 18:34:03 +0100

    inital setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container d-flex flex-column">
+    <h2 class="text-center">
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+      {{ msg }}
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+    </h2>
+    <img src="@/assets/logo.svg" alt="Vue logo" height="100" />
+  </div>
+  <SayHello name="Robert"></SayHello>
+</template>
+
+<script setup>
+import SayHello from '@/components/SayHello.vue';
+
+const msg = 'Willkommen!';
+</script>
+
+<style>
+@import '@/assets/bootstrap.min.css';
+</style>
---
+++ src/components/SayHello.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p class="text-center fs-5 fst-italic">{{ name }} sagt: Hallo!</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+});
+</script>
